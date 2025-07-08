04e09e5e2e057ea3a377a5a4fc3e7fa1eccda8e9
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-08 10:01:36 -0300

    first commit

---
+++ src/main/java/frutuoso/java10x/EnventClean/EnventCleanApplication.java
@@ -0,0 +1,13 @@
+package frutuoso.java10x.EnventClean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnventCleanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnventCleanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/frutuoso/java10x/EnventClean/EnventCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package frutuoso.java10x.EnventClean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnventCleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
