10ca82f53f827e8513ec39f43251b6794bede196
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-13 12:35:35 -0300

    feat: add beans

--- src/main/java/frutuoso/java10x/EventClean/infra/beans/BeansConfig.java
+++ src/main/java/frutuoso/java10x/EventClean/infra/beans/BeansConfig.java
@@ -2,6 +2,8 @@ package frutuoso.java10x.EventClean.infra.beans;
 import frutuoso.java10x.EventClean.core.gateway.EventGateway;
 import frutuoso.java10x.EventClean.core.useCases.CreateEventCase;
 import frutuoso.java10x.EventClean.core.useCases.CreateEventCaseImpl;
+import frutuoso.java10x.EventClean.core.useCases.SearchEventCase;
+import frutuoso.java10x.EventClean.core.useCases.SearchEventCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +13,10 @@ public class BeansConfig {
     @Bean
     public CreateEventCase createEventCase(EventGateway eventGateway) {
         return new CreateEventCaseImpl(eventGateway);
+    }
+
+    @Bean
+    public SearchEventCase searchEventCase(EventGateway eventGateway){
+        return new SearchEventCaseImpl(eventGateway);
     }
 }
