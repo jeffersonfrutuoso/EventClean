8dfef68c31b95060e47f3c8f282a5c0d6e7d8806
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-13 12:33:25 -0300

    feat: add gateway do core da aplicação

--- src/main/java/frutuoso/java10x/EventClean/core/gateway/EventGateway.java
+++ src/main/java/frutuoso/java10x/EventClean/core/gateway/EventGateway.java
@@ -1,7 +1,10 @@
 package frutuoso.java10x.EventClean.core.gateway;
-
 import frutuoso.java10x.EventClean.core.entities.Event;
+import java.util.List;
+import java.util.Optional;
 
 public interface EventGateway {
      public Event createEvent(Event event);
+
+     public List<Event> listEvents();
 }
