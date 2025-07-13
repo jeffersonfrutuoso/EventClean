7146e8a3172d86144eade63e3029bd6ba7cca880
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-13 12:36:27 -0300

    feat: add controller

--- src/main/java/frutuoso/java10x/EventClean/infra/controller/EventController.java
+++ src/main/java/frutuoso/java10x/EventClean/infra/controller/EventController.java
@@ -1,19 +1,25 @@
 package frutuoso.java10x.EventClean.infra.controller;
 import frutuoso.java10x.EventClean.core.entities.Event;
 import frutuoso.java10x.EventClean.core.useCases.CreateEventCase;
+import frutuoso.java10x.EventClean.core.useCases.SearchEventCase;
 import frutuoso.java10x.EventClean.infra.dtos.EventDto;
 import frutuoso.java10x.EventClean.infra.mapper.EventDtoMapper;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("event/")
 public class EventController {
     private final CreateEventCase createEventCase;
     private final EventDtoMapper eventDtoMapper;
+    private final SearchEventCase searchEventCase;
 
-    public EventController(CreateEventCase createEventCase, EventDtoMapper eventDtoMapper) {
+    public EventController(CreateEventCase createEventCase, EventDtoMapper eventDtoMapper, SearchEventCase searchEventCase) {
         this.createEventCase = createEventCase;
         this.eventDtoMapper = eventDtoMapper;
+        this.searchEventCase = searchEventCase;
     }
 
     @PostMapping("/create")
@@ -22,8 +28,11 @@ public class EventController {
         return eventDtoMapper.toEventDto(newEvent);
     }
 
-    @GetMapping
-    public String showEvents() {
-        return "eventos";
+    @GetMapping("/listar")
+    public List<EventDto> execute() {
+        return searchEventCase.execute()
+                .stream()
+                .map(eventDtoMapper::toEventDto)
+                .collect(Collectors.toList());
     }
 }
