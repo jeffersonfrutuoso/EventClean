838820e9fb933034f28d69ce1935c0df53a73ead
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-14 12:36:03 -0300

    feat: add execute na interface do usecase, e adiciona a exception notFound no use case de implements de filtrar por nome

--- src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterNameCase.java
+++ src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterNameCase.java
@@ -1,4 +1,7 @@
 package frutuoso.java10x.EventClean.core.useCases;
 
+import frutuoso.java10x.EventClean.core.entities.Event;
+
 public interface FilterNameCase {
+    public Event execute(String name);
 }
--- src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterNameCaseImpl.java
+++ src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterNameCaseImpl.java
@@ -1,4 +1,20 @@
 package frutuoso.java10x.EventClean.core.useCases;
 
-public class FilterNameCaseImpl {
+import frutuoso.java10x.EventClean.core.entities.Event;
+import frutuoso.java10x.EventClean.core.gateway.EventGateway;
+import frutuoso.java10x.EventClean.infra.exceptions.NotFoundException;
+
+public class FilterNameCaseImpl implements FilterNameCase{
+
+    private final EventGateway eventGateway;
+
+    public FilterNameCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+    @Override
+    public Event execute(String name) {
+        return eventGateway.filterForName(name)
+                .orElseThrow(() -> new NotFoundException("Evento com nome: " + name + " não encontrado"));
+    }
 }
