27d0bb98814c5d2aaa1feda09c776b5e27328cda
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-14 12:42:55 -0300

    feat: add findby name e identificador no event repository para usar no gateway do infra

--- src/main/java/frutuoso/java10x/EventClean/infra/persistence/EventRepository.java
+++ src/main/java/frutuoso/java10x/EventClean/infra/persistence/EventRepository.java
@@ -1,6 +1,12 @@
 package frutuoso.java10x.EventClean.infra.persistence;
 
+import frutuoso.java10x.EventClean.core.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+    Optional<Event> findByIdentifier(String identifier);
+    Optional<Event> findByName(String name);
 }
