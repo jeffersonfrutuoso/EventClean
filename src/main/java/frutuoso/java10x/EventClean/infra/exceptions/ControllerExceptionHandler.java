e9cbd8f550254e48ac489c55ebe5b8717aaf4772
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-14 12:38:33 -0300

    feat: add o controlador das exceptions que eu criei para os eventos

--- src/main/java/frutuoso/java10x/EventClean/infra/exceptions/ControllerExceptionHandler.java
+++ src/main/java/frutuoso/java10x/EventClean/infra/exceptions/ControllerExceptionHandler.java
@@ -1,4 +1,28 @@
 package frutuoso.java10x.EventClean.infra.exceptions;
 
-public class ControllerExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler{
+
+    @ExceptionHandler(DuplicateEventException.class)
+    public ResponseEntity<Map<String, String>> handlerDuplicateException (DuplicateEventException exception) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", exception.getMessage());
+        response.put("mensagem", "o identificador ja existe");
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlerNotFoundException (NotFoundException exception) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", exception.getMessage());
+        response.put("mensagem", "o identificador não foi encontrado, tente novamente");
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
 }
