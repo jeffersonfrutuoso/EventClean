bc3d726b9945cf8052a4d9ad234068c4e5dd93aa
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-14 12:34:38 -0300

    feat: add execute na interface do usecase, e adiciona a exception notFound no use case de implements

--- src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterIdentifierCaseImpl.java
+++ src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterIdentifierCaseImpl.java
@@ -1,4 +1,20 @@
 package frutuoso.java10x.EventClean.core.useCases;
 
-public class FilterIdentifierCaseImpl {
+import frutuoso.java10x.EventClean.core.entities.Event;
+import frutuoso.java10x.EventClean.core.gateway.EventGateway;
+import frutuoso.java10x.EventClean.infra.exceptions.NotFoundException;
+
+public class FilterIdentifierCaseImpl implements FilterIdentifierCase{
+
+    private final EventGateway eventGateway;
+
+    public FilterIdentifierCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+    @Override
+    public Event execute(String identifier) {
+        return eventGateway.filterForIdentifier(identifier)
+                .orElseThrow(() -> new NotFoundException("Evento com identificador: " + identifier + " não encontrado"));
+    }
 }
