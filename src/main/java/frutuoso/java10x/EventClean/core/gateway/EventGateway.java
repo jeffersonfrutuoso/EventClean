5a59f13de829889b37f2e4f76a23e7cb1cfacc8b
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-14 12:29:47 -0300

    feat: add contrato de filtragem de nome, identificador, e se o identificador ja existe

--- src/main/java/frutuoso/java10x/EventClean/core/gateway/EventGateway.java
+++ src/main/java/frutuoso/java10x/EventClean/core/gateway/EventGateway.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EventGateway {
+     Optional<Event> filterForIdentifier(String identifier);
+     Optional<Event> filterForName(String name);
      public Event createEvent(Event event);
-
      public List<Event> listEvents();
+     boolean indentifierExists(String identifier);
 }
