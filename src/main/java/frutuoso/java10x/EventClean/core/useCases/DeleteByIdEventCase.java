6322dee843c3e1ca16895efe808c52c260a9e0c7
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-15 11:58:36 -0300

    feat: add interface do use case de deletar por id

--- src/main/java/frutuoso/java10x/EventClean/core/useCases/DeleteByIdEventCase.java
+++ src/main/java/frutuoso/java10x/EventClean/core/useCases/DeleteByIdEventCase.java
@@ -1,4 +1,7 @@
 package frutuoso.java10x.EventClean.core.useCases;
 
+import frutuoso.java10x.EventClean.core.entities.Event;
+
 public interface DeleteByIdEventCase {
+    public void execute(Long id);
 }
