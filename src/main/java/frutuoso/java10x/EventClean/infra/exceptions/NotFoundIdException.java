3a577c13bf196d3c59340b3b5bfa3b8c11b540e8
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-15 11:10:44 -0300

    feat: cria a exception de filtrar por id

--- src/main/java/frutuoso/java10x/EventClean/infra/exceptions/NotFoundIdException.java
+++ src/main/java/frutuoso/java10x/EventClean/infra/exceptions/NotFoundIdException.java
@@ -1,4 +1,7 @@
 package frutuoso.java10x.EventClean.infra.exceptions;
 
-public class NotFoundIdException {
+public class NotFoundIdException extends RuntimeException{
+    public NotFoundIdException(String message){
+        super(message);
+    }
 }
