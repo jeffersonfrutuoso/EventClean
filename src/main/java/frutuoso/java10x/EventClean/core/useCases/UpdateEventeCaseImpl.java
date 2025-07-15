8bcc6549f908a63f80b58c5c1149d09588f71ff0
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-15 12:48:18 -0300

    feat: add use cases de atualizar evento

--- src/main/java/frutuoso/java10x/EventClean/core/useCases/UpdateEventeCaseImpl.java
+++ src/main/java/frutuoso/java10x/EventClean/core/useCases/UpdateEventeCaseImpl.java
@@ -1,4 +1,18 @@
 package frutuoso.java10x.EventClean.core.useCases;
 
-public class UpdateEventeCaseImpl {
+import frutuoso.java10x.EventClean.core.entities.Event;
+import frutuoso.java10x.EventClean.core.gateway.EventGateway;
+
+public class UpdateEventeCaseImpl implements UpdateEventCase{
+
+    private final EventGateway eventGateway;
+
+    public UpdateEventeCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+    @Override
+    public Event execute(Event event, Long id) {
+        return eventGateway.updatedEvent(event,id);
+    }
 }
