b9493094441a3f5c6c4e069632c4d18ef2d4186c
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-15 11:03:36 -0300

    feat: add o metodo de filtragem por id

--- src/main/java/frutuoso/java10x/EventClean/core/gateway/EventGateway.java
+++ src/main/java/frutuoso/java10x/EventClean/core/gateway/EventGateway.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public interface EventGateway {
      Optional<Event> filterForIdentifier(String identifier);
      Optional<Event> filterForName(String name);
+     Optional<Event> filterById(Long id);
      public Event createEvent(Event event);
      public List<Event> listEvents();
      boolean indentifierExists(String identifier);
