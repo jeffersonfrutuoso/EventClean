3f51579cb1d2ad5bb7399b293eef342c72541b9f
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-07-15 11:05:43 -0300

    feat: add execute da interface do use case de filtrar por id e faz a implementação, assina o contrato da interface na implementação do use case defiltrar por id

--- src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterByIdEventCase.java
+++ src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterByIdEventCase.java
@@ -1,4 +1,9 @@
 package frutuoso.java10x.EventClean.core.useCases;
 
+import frutuoso.java10x.EventClean.core.entities.Event;
+
+import java.util.Optional;
+
 public interface FilterByIdEventCase {
+    public Event execute(Long id);
 }
--- src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterByIdEventCaseImpl.java
+++ src/main/java/frutuoso/java10x/EventClean/core/useCases/FilterByIdEventCaseImpl.java
@@ -1,4 +1,22 @@
 package frutuoso.java10x.EventClean.core.useCases;
 
-public class FilterByIdEventCaseImpl {
+import frutuoso.java10x.EventClean.core.entities.Event;
+import frutuoso.java10x.EventClean.core.gateway.EventGateway;
+import frutuoso.java10x.EventClean.infra.exceptions.NotFoundIdException;
+
+import java.util.Optional;
+
+public class FilterByIdEventCaseImpl implements FilterByIdEventCase{
+
+    private final EventGateway eventGateway;
+
+    public FilterByIdEventCaseImpl(EventGateway eventGateway) {
+        this.eventGateway = eventGateway;
+    }
+
+    @Override
+    public Event execute(Long id) {
+        return eventGateway.filterById(id)
+                .orElseThrow(() -> new NotFoundIdException("Evento com o id " + id + " não encontrado"));
+    }
 }
